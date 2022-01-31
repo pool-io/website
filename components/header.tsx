a0183f50c550e4d8193356f99a1c3a1abcdd4386
Soung Bae Kim <sbkim@berkeley.edu>
2022-01-30 16:50:58 -0800

    favicon

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import styles from './header.module.css'
+
+export default function Header() {
+    return (
+        <div className={styles.container}>
+            poolet.io
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,22 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../components/layout'
+import Header from '../components/header'
+
 export default function Home() {
-    return <h1>First Post</h1>
+    return (
+        <div className=''>
+            <Head>
+                <title>poolet.io</title>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png"/>
+            </Head>
+            <Header></Header>
+
+            <h1>poolet.io</h1>
+            <p>One place to manage all your finances</p>
+            <Link href="/about">
+                <a>learn more</a>
+            </Link>
+        </div>
+    )
 }

af652867f9d0db5480b082fb0ccb1f0d4dcd8e02
Soung Bae Kim <sbkim@berkeley.edu>
2022-03-24 06:47:01 -0700

    fix header collision

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -4,7 +4,17 @@ import Layout from '@components/layout';
 export default function Blog() {
     return (
         <Layout route="/blog">
-            <h1>Blog</h1>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    marginTop: 100,
+                    width: '100%',
+                    height: '100%'
+                }}
+            >
+                <h1>Blog</h1>
+            </div>
         </Layout>
     );
 }
--- pages/learn.tsx
+++ pages/learn.tsx
@@ -4,7 +4,17 @@ import Layout from '@components/layout';
 export default function Learn() {
     return (
         <Layout route="/learn">
-            <h1>Learn</h1>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    marginTop: 100,
+                    width: '100%',
+                    height: '100%'
+                }}
+            >
+                <h1>Learn</h1>
+            </div>
         </Layout>
     );
 }

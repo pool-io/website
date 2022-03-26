ea8c0095ac738fd45bbe591822d3b7357ddc159f
Soung Bae Kim <sbkim@berkeley.edu>
2022-03-26 12:47:11 -0700

    init portal

---
+++ pages/portal.tsx
@@ -0,0 +1,15 @@
+export default function Portal() {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                width: '100vw'
+            }}
+        >
+            <h1>portal</h1>
+        </div>
+    );
+}
---
+++ portal/firebase.tsx
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import {
+    FirebaseAppSettings,
+    FirebaseOptions,
+    initializeApp
+} from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig: FirebaseOptions = {
+    apiKey: 'AIzaSyBS2pvLCshpIlCLCSMRwoG4ALNQEVS6HDw',
+    authDomain: 'pool-c23a1.firebaseapp.com',
+    projectId: 'pool-c23a1',
+    storageBucket: 'pool-c23a1.appspot.com',
+    messagingSenderId: '940489172024',
+    appId: '1:940489172024:web:76cc14c22963663d42f2cd',
+    measurementId: 'G-2EKKG29H77'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

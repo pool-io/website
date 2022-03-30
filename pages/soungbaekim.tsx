1252793dbcfe65f76cc2ec29549b33c319e62b89
Soung Bae Kim <sbkim@berkeley.edu>
2022-03-29 22:47:37 -0700

    initial sb page

---
+++ pages/soungbaekim.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import Layout from '@components/layout';
+
+export default function SoungBaeKim() {
+    return (
+        <Layout route="/learn">
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                    marginTop: 100,
+                    width: '100vw',
+                    height: '100vh'
+                }}
+            >
+                <h1>Soung Bae Kim</h1>
+                <p>sbkim@berkeley.edu</p>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        width: '50vw'
+                    }}
+                >
+                    <div
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'column'
+                        }}
+                    >
+                        <h2>fwaygo</h2>
+                    </div>
+                    <div>
+                        <h2>projectulterior</h2>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+}

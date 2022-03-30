9894ff0e6ddeca18a78dfd01a28b94dd9f2eabb4
Soung Bae Kim <sbkim@berkeley.edu>
2022-03-29 22:31:06 -0700

    remove portal

--- pages/portal.tsx
+++ pages/portal.tsx
@@ -1,14 +1,19 @@
 import App from '@app/App';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { Duplex } from 'stream';
 
 export default function Portal() {
-    const router = useRouter();
-    // const [isSignUp, setIsSignUp] = useState<boolean>(false);
-
-    // useEffect(() => {
-    console.log('router:', router.query['signup']);
-    //     setIsSignUp(Boolean(router.query['signup']));
-    // }, []);
-    return <App isSignUp={router.query['signup'] !== undefined} />;
+    return <div />;
 }
+
+// export default function Portal() {
+//     const router = useRouter();
+//     // const [isSignUp, setIsSignUp] = useState<boolean>(false);
+
+//     // useEffect(() => {
+//     console.log('router:', router.query['signup']);
+//     //     setIsSignUp(Boolean(router.query['signup']));
+//     // }, []);
+//     return <App isSignUp={router.query['signup'] !== undefined} />;
+// }

e7738c57315b7489465be709b255a243ae341de5
Brian Kim <2kimbrian1@gmail.com>
2022-09-26 23:04:54 -0700

    bralley update

--- pages/about/bralley.tsx
+++ pages/about/bralley.tsx
@@ -7,32 +7,17 @@ export default function Learn() {
             <div
                 style={{
                     display: 'flex',
-                    flexDirection: 'column',
+                    flexDirection: 'row',
                     justifyContent: 'flex-start',
-                    alignItems: 'center',
-                    marginTop: 100,
-                    width: '100vw',
-                    height: '100vh'
+                    alignItems: 'left',
+                    width: '100%',
+                    height: '100%',
+                    fontSize: '20px',
+                    backgroundColor: 'powderblue'
                 }}
             >
-                <h1>cum bucket</h1>
-                <p>lemme get a sip</p>
-                <div
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        justifyContent: 'flex-start',
-                        alignItems: 'center',
-                        marginTop: 100,
-                        width: '100vw',
-                        height: '100vh'
-                    }}
-                >
-                    <h1>mmmmm pEniS</h1>
-                </div>
-                <div style={{ height: 100 }} />
-                <h1>you looked gayass</h1>
-                <Image src="/images/penis.jpg" width={400} height={1000} />
+                <h1>About Us</h1>
+                <Image src="/images/phone.png" width={400} height={400} />
             </div>
         </Layout>
     );
